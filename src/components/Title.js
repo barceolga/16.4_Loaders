76a3ab6e356ccbd8e499c6cf432bedf6a3a4a6a8
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-05 15:07:40 +0200

    Add src folder with components folder and container folder inside with all the components files

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Title.css';
+
+const Title = props => {
+      return (
+        <div className={style.Title}>
+            <h1>{props.title}</h1>
+            <p>Number of task to do:<span> {props.number}</span></p>
+        </div>
+      );
+}
+
+export default Title;
