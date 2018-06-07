a26269d45f84dd60df931bd87d6cf7b17d901f7a
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-07 20:41:59 +0200

    Update content for webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+const plugins = [new HtmlWebpackPlugin({
+  template: 'src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})];
 //webpack.config.js
 
 module.exports = (env) => {
+  if (env === 'production') {
+    plugins.push(
+        new OptimizeJsPlugin({
+          sourceMap: false
+        })
+    )
+  }
   const environment = env || 'production';
   return {
     mode: environment,
     entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'build'),
-      filename: 'app.' + environment + 'bundle.js'
+      filename: 'app.bundle.js'
     },
       module: {
         rules: [
@@ -28,9 +41,25 @@ module.exports = (env) => {
                 }
               }
           ]
+        },
+        {
+          test: /\.scss$/,
+          use: [
+              {loader: 'style-loader'},
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                }
+          ]
         }
       ]
-    }
+    },
+    plugins: plugins
   }
   optimization: {
     minimize: true
