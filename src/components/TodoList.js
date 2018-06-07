5906d1bae08aee0d632897b71e085ce4dfe6df89
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-07 20:40:48 +0200

    Create TodoList component for TodoList.js file

---
+++ src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {array} from 'prop-types';
+import Todo from './Todo.js';
+import style from './TodoList.css';
+
+const TodoList = props => {
+
+  return(
+    <ol className={style.TodoList}>
+    {props.list.map(item => {
+      return <Todo item={item} key={item.id} removeItem={props.removeItem}></Todo>
+    })
+  }
+    </ol>
+  )
+}
+TodoList.propTypes = {list: array};
+export default TodoList;
