08bbac3a7dbce313b4dd20ae3b2d40529bbb7c4a
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-07 07:34:29 +0200

    Update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'build'),
-      filename: 'app.bundle.js'
+      filename: 'app.' + environment + 'bundle.js'
     },
       module: {
         rules: [
