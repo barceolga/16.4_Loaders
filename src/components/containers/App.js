e84d996c96ba6c071d58be03daca309c72c68f8f
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-07 20:41:33 +0200

    Update content for App.js file

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -3,14 +3,37 @@ import ReactDOM from 'react-dom';
 import uuid from 'uuid';
 import style from './App.css';
 import Title from '../Title.js';
+import TodoList from '../TodoList.js';
 
 class App extends React.Component {
     constructor(props){
       super(props);
       this.state = {
-        data: []
+        data: [
+          {
+            id: 1,
+            text: 'Clear room'
+          },
+          {
+            id: 2,
+            text: 'Wash the dishes'
+          },
+          {
+            id: 3,
+            text: 'Walk my dog'
+          },
+          {
+            id: 4,
+            text: 'Do the shopping'
+          },
+          {
+            id: 5,
+            text: 'Prepare the supper'
+          }
+        ]
       };
     }
+
     addTodo(val){
       const todo = {
         text: val,
@@ -19,14 +42,18 @@ class App extends React.Component {
       const data = [...this.state.data, todo];
       this.setState({data});
     }
-    removeTodo(id) {
-      const raminder = this.state.data.filter(todo => todo.id !== id);
-      this.setState({data: remainder});
+
+    removeItem = (id) => {
+      this.setState({
+        data: this.state.data.filter(item => item.id !== id)
+      });
     }
+
     render() {
       return (
         <div className={style.TodoApp}>
             <Title title="ToDo list App" number={this.state.data.length}></Title>
+            <TodoList list={this.state.data} removeItem={this.removeItem}></TodoList>
         </div>
       );
     }
